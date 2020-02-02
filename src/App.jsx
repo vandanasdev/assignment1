aa49dbc765dabc319f6cd3601f36a84a24d7fada
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-02-02 15:23:07 -0800

    Updated Intro, added name and intro class styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 
 const element = (
     <div className="center">
-        <div className="name">Vandana Singh</div>
+        <div className="name">VANDANA SINGH</div>
         <img className="image" src='/images/vandanaProfilePic.png' />
-        <div> I am an extended studies Student</div>
+        <div className="intro"> I am pursuing my Master's in Computer Science at SDSU. 
+                    In the past, I have worked as Quality Assurance Engineer at HiveMedia Group, Carlsbad.
+                    I am planning to get into Full Stack Web developer role after I complete my Masters.
+        </div>
         <div className="button-div">
             <a className="button" href="https://github.com/vandanasdev/assignment1">VIEW MY GITHUB REPO</a>
         </div>
