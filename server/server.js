897af769536c897c726a88891b8fe1eb8c9643e8
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-02-02 04:20:13 -0800

    test

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(3000, function(){
     console.log('App started on port 3000');
