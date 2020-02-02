09990ed88aed617f9ca5e722c4f7e1353d7bfd4c
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-02-02 02:17:11 -0800

    Added image tag in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 const element = (
     <div title="Outer div">
         <div>Vandana Singh</div>
+        <img src={ require('./images/vandanaProfilePic.jpg') } />
         <div> I am an extended studies Student</div>
         <a href="https://github.com/vandanasdev/assignment1">VIEW MY GITHUB REPO</a>
     </div>
